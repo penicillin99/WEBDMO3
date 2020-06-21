1700313368b38921a2069a1b966afb9bc6cab75c
penicillin99 <56688043+penicillin99@users.noreply.github.com>
2020-06-21 09:55:56 +0700

    update
    update back-end gồm các chức năng quản lý tài khoản, quản lý nhân viên, quản lý loại phòng

---
+++ WEBDMO3/Areas/Admin/Controllers/EMPLOYERController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Models.EF;
+
+namespace WEBDMO3.Areas.Admin.Controllers
+{
+    public class EMPLOYERController : BaseController
+    {
+        private WebDbContext db = new WebDbContext();
+
+        // GET: Admin/EMPLOYER
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYERs.ToList());
+        }
+
+        // GET: Admin/EMPLOYER/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYER eMPLOYER = db.EMPLOYERs.Find(id);
+            if (eMPLOYER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYER);
+        }
+
+        // GET: Admin/EMPLOYER/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/EMPLOYER/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,fullname,address,phoneNumber")] EMPLOYER eMPLOYER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYERs.Add(eMPLOYER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eMPLOYER);
+        }
+
+        // GET: Admin/EMPLOYER/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYER eMPLOYER = db.EMPLOYERs.Find(id);
+            if (eMPLOYER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYER);
+        }
+
+        // POST: Admin/EMPLOYER/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,fullname,address,phoneNumber")] EMPLOYER eMPLOYER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYER);
+        }
+
+        // GET: Admin/EMPLOYER/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYER eMPLOYER = db.EMPLOYERs.Find(id);
+            if (eMPLOYER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYER);
+        }
+
+        // POST: Admin/EMPLOYER/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EMPLOYER eMPLOYER = db.EMPLOYERs.Find(id);
+            db.EMPLOYERs.Remove(eMPLOYER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WEBDMO3/Areas/Admin/Controllers/MANAGERController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Models.EF;
+
+namespace WEBDMO3.Areas.Admin.Controllers
+{
+    public class MANAGERController : BaseController
+    {
+        private WebDbContext db = new WebDbContext();
+
+        // GET: Admin/MANAGER
+        public ActionResult Index()
+        {
+            return View(db.MANAGERs.ToList());
+        }
+
+        // GET: Admin/MANAGER/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MANAGER mANAGER = db.MANAGERs.Find(id);
+            if (mANAGER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mANAGER);
+        }
+
+        // GET: Admin/MANAGER/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/MANAGER/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,username,password,role")] MANAGER mANAGER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MANAGERs.Add(mANAGER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mANAGER);
+        }
+
+        // GET: Admin/MANAGER/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MANAGER mANAGER = db.MANAGERs.Find(id);
+            if (mANAGER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mANAGER);
+        }
+
+        // POST: Admin/MANAGER/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,username,password,role")] MANAGER mANAGER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mANAGER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mANAGER);
+        }
+
+        // GET: Admin/MANAGER/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MANAGER mANAGER = db.MANAGERs.Find(id);
+            if (mANAGER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mANAGER);
+        }
+
+        // POST: Admin/MANAGER/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MANAGER mANAGER = db.MANAGERs.Find(id);
+            db.MANAGERs.Remove(mANAGER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
